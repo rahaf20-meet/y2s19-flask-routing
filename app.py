0145517ab8bec79cbd5d282d559c7dfe746bc1f2
myname21-meet <myname21@meet.mit.edu>
2019-07-30 10:49:10 -0400

    flask rouing

--- app.py
+++ app.py
@@ -6,11 +6,10 @@ app = Flask(__name__)
 def home():
 	students=query_all()
 	return render_template('home.html', students=students)
-	return "home page" 
 
 @app.route('/student/<int:student_id>')
 def display_student(student_id):
-	return render_template("student.html", id_number=student_id, home=home, student=query_by_id(student_id))
+	return render_template("student.html", id_number=student_id, student=query_by_id(student_id))
 
 if __name__ == '__main__':
     app.run(debug=True)
